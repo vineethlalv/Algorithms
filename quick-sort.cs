f3d26fe75b6f72df22b760f32cc4932f57765f65
vineethlalv <vineethlal.v@outlook.com>
2022-07-19 09:39:34 +0530

    quick sort

---
+++ quick-sort.cs
@@ -0,0 +1,39 @@
+namespace algorithms.sort;
+
+class QuickSort
+{
+    public int[] qsort(int[] arr, int p, int r)
+    {
+        if(p < r)
+        {
+            int q = partition(arr, p, r);
+            qsort(arr, p, q-1);
+            qsort(arr, q+1, r);
+        }
+
+        return arr;
+    }
+
+    private int partition(int[] arr, int p, int r)
+    {
+        // select an element as pivot and move all lesser and equal to left and greater to right
+
+        int q = r;
+        int pivot = arr[r];
+
+        for(int i = p; i < q; i++)
+        {
+            if(arr[i] > pivot)
+            {
+                int temp = arr[--q];
+                arr[q] = arr[i];
+                arr[i] = temp;
+                --i;
+            }
+        }
+        arr[r] = arr[q];
+        arr[q] = pivot;
+
+        return q;
+    }
+}
